731f36d5b552ea828a97a3eee4865179359bcae4
joshy91 <joshua.davidsonsga@gmail.com>
2018-08-27 12:00:46 -0500

    Adjusted about page photos and contact email to owner

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ApplicationMailer
-default from: ENV['support_email']
+default from: ENV['OWNER_EMAIL']
 
 def contact_email
 @contact = params[:contact]
 mg_client = Mailgun::Client.new ENV['mail_gun_key']
-message_params = {:from    => ENV['support_email'],
+message_params = {:from    => ENV['OWNER_EMAIL'],
                   :to      => @contact.email,
                   :subject => 'Sample Mail using Mailgun API',
                   :text    => 'You received a message from ' + @contact.name + ' with email address ' + @contact.email + ' and phone number ' + @contact.phone + '.
