63111bbc82f1e0d1f7091ce5bdea0804e1dc2d62
joshy91 <joshua.davidsonsga@gmail.com>
2018-09-10 13:30:17 -0700

    Removed turbo links and adjusted pages to include yukon photos for suppliers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require popper.min
 //= require bootstrap
 //= require mdb
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
