37ba783b23de92461ab001f3b7c63389a158e3cc
joshy91 <joshua.davidsonsga@gmail.com>
2018-10-03 15:06:22 -0500

    Adjust metatags to improve SEO

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -12,5 +12,15 @@ The visitor said: ' + @contact.message }
 mg_client.send_message ENV['mail_gun_domain'], message_params
 
 end
+
+def send_simple_message
+	RestClient.post "https://api:YOUR_API_KEY" 
+	"@api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+	:from => "Excited User <mailgun@YOUR_DOMAIN_NAME>",
+	:to => "bar@example.com, YOU@YOUR_DOMAIN_NAME",
+	:subject => "Hello",
+	:text => "Testing some Mailgun awesomness!"
+end
+
 end
 
